63f3043898ed30446593c73d566201c835543cd4
Andreacolaci99 <colaciandrea99@gmail.com>
2025-02-27 18:58:34 +0100

    feat : Concerto

--- src/Milestone/Concerto.java
+++ src/Milestone/Concerto.java
@@ -1,38 +1,55 @@
 package Milestone;
 
+import java.text.DecimalFormat;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
-public class Concerto extends Evento{
+public class Concerto extends Evento {
 
-    LocalTime orario;
-    double prezzo;
-    DateTimeFormatter dataFormattata = DateTimeFormatter.ofPattern("EEEE, dd MMMM yyyy");
+    private LocalTime ora;
+    private double prezzo;
+    
+    private static final DateTimeFormatter dataFormattata = DateTimeFormatter.ofPattern("dd MMMM yyyy");
+    private static final DateTimeFormatter oraFormattata = DateTimeFormatter.ofPattern("HH:mm");
+    private static final DecimalFormat prezzoFormattato = new DecimalFormat("##,##0.00€");
 
-
-    public Concerto(String titolo, LocalDate dataEvento, int postiTotali, LocalTime orario, double prezzo){
+    public Concerto(String titolo, LocalDate dataEvento, int postiTotali, LocalTime ora, double prezzo) {
         super(titolo, dataEvento, postiTotali);
+        this.ora = ora;
+        this.prezzo = prezzo;
     }
 
-
-    public LocalTime getOrario() {
-        return orario;
+    public LocalTime getOra() {
+        return ora;
     }
 
-
-    public void setOrario(LocalTime orario) {
-        this.orario = orario;
+    public void setOra(LocalTime ora) {
+        this.ora = ora;
     }
-
 
     public double getPrezzo() {
         return prezzo;
     }
 
-
     public void setPrezzo(double prezzo) {
         this.prezzo = prezzo;
     }
-    
+
+    public String getDataFormattata() {
+        return super.getData().format(dataFormattata);
+    }
+
+    public String getOraFormattata() {
+        return ora.format(oraFormattata);
+    }
+
+    public String getPrezzoFormattato() {
+        return prezzoFormattato.format(prezzo);
+    }
+
+    @Override
+    public String toString() {
+        return getDataFormattata() + " alle " + getOraFormattata() + " - " + super.getTitolo() + " - " + getPrezzoFormattato();
+    }
 }
